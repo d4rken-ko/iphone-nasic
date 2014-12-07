d53daf123728ddd9d15db8f9c0a2a239fda44c84
darken <d4rken@gmail.com>
2014-12-07 04:41:33 +0100

    Walls!, Bouncy walls! And a dragable player with some physics

--- NASIC/GameScene.swift
+++ NASIC/GameScene.swift
@@ -17,11 +17,12 @@ class GameScene: SKScene {
         /* Setup your scene here */
 
         backgroundColor = SKColor(rgba: "#2D2D2D")
+        let worldBody = SKPhysicsBody (edgeLoopFromRect: self.frame)
+        worldBody.restitution = 0.3
+        self.physicsBody = worldBody
 
-        let player = SKSpriteNode(imageNamed: "apple_rotated1")
-        player.name = "Player"
-        player.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMinY(self.frame)+player.frame.height+10);
-        player.runAction(SKAction.scaleBy(0.5, duration: 0))
+
+        let player = makePlayer()
         addChild(player)
 
       //  let testBlock = SKSpriteNode(color: SKColor.blueColor(), size: CGSize(width: self.frame.width - 5, height: self.frame.height - 5))
@@ -30,6 +31,20 @@ class GameScene: SKScene {
 
         let androidBlock = makeAndroidBlock()
         addChild(androidBlock)
+    }
+
+    func makePlayer() -> SKSpriteNode {
+        let player = SKSpriteNode(imageNamed: "apple_rotated1")
+        player.name = "Player"
+        player.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMinY(self.frame)+player.frame.height+10);
+        player.runAction(SKAction.scaleBy(0.5, duration: 0))
+        player.physicsBody = SKPhysicsBody(rectangleOfSize: player.frame.size)
+        player.physicsBody?.dynamic = true
+        player.physicsBody?.affectedByGravity = false
+        player.physicsBody?.mass = 0.7
+        player.physicsBody?.linearDamping = 2.0
+        player.physicsBody?.angularDamping = 1.0
+        return player
     }
 
 
@@ -90,28 +105,40 @@ class GameScene: SKScene {
         self.lastAndroidsMovement = currentTime
     }
     
+    var lastTouch: CGPoint? = nil
+
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        let touch = touches.anyObject() as UITouch
+        let touchLocation = touch.locationInNode(self)
+        lastTouch = touchLocation
     }
-   
+
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        let touch = touches.anyObject() as UITouch
+        let touchLocation = touch.locationInNode(self)
+        lastTouch = touchLocation
+    }
+
+    // Be sure to clear lastTouch when touches end so that the impulses stop being applies
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        lastTouch = nil
+    }
+
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
         updateAndroidsPosition(currentTime)
+        // Based on: http://stackoverflow.com/questions/25277956/move-a-node-to-finger-using-swift-spritekit
+        // Only add an impulse if there's a lastTouch stored
+        let player = self.childNodeWithName("Player") as SKSpriteNode
+        if let touch = lastTouch {
+            let impulseVector = CGVector(dx: (touch.x - player.position.x)/2, dy: 0)
+            // If myShip starts moving too fast or too slow, you can multiply impulseVector by a constant or clamp its range
+            player.physicsBody?.applyImpulse(impulseVector)
+        } else if !player.physicsBody!.resting {
+            // Adjust the -0.5 constant accordingly
+            let impulseVector = CGVector(dx: player.physicsBody!.velocity.dx * -1.0,dy: 0)
+            player.physicsBody?.applyImpulse(impulseVector)
+        }
     }
+
 }
