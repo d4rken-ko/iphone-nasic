99e9bc869efbac64ea116c45ea5fd2f481a14891
darken <d4rken@gmail.com>
2014-12-07 03:43:36 +0100

    Android on the move

--- NASIC/GameScene.swift
+++ NASIC/GameScene.swift
@@ -10,20 +10,82 @@ import SpriteKit
 
 class GameScene: SKScene {
 
-    let player = SKSpriteNode(imageNamed: "apple_rotated1")
+
+
 
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Helvetica Thin")
-        myLabel.text = "Ready?";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        self.addChild(myLabel)
 
         backgroundColor = SKColor(rgba: "#2D2D2D")
 
-        player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
+        let player = SKSpriteNode(imageNamed: "apple_rotated1")
+        player.name = "Player"
+        player.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMinY(self.frame)+player.frame.height+10);
+        player.runAction(SKAction.scaleBy(0.5, duration: 0))
         addChild(player)
+
+      //  let testBlock = SKSpriteNode(color: SKColor.blueColor(), size: CGSize(width: self.frame.width - 5, height: self.frame.height - 5))
+      //  testBlock.position = CGPoint(x:CGRectGetMidX(self.frame), y: CGRectGetMidY(self.frame))
+      //  addChild(testBlock)
+
+        let androidBlock = makeAndroidBlock()
+        addChild(androidBlock)
+    }
+
+
+    func makeAndroidBlock() -> SKNode {
+        let _androidBlock = SKNode()
+        _androidBlock.name = "Androids"
+        for column in 1...6 {
+            for row in 1...5 {
+                let android = SKSpriteNode(imageNamed: "android_head1")
+                android.name = "Android"
+                android.runAction(SKAction.scaleBy(0.5, duration: 0))
+                android.position = CGPoint(x:CGFloat(35*column), y:CGFloat(35*row))
+                _androidBlock.addChild(android)
+            }
+        }
+        let blockSize = _androidBlock.calculateAccumulatedFrame()
+        _androidBlock.position = CGPoint(x:(CGRectGetMidX(self.frame) - CGRectGetMidX(blockSize)), y:(CGRectGetMaxY(self.frame) - CGRectGetHeight(blockSize) - CGFloat(150)));
+
+
+        //let testBlock = SKSpriteNode(color: SKColor.redColor(), size: CGSize(width: blockSize.width, height: blockSize.height))
+        //_androidBlock.addChild(testBlock)
+        //testBlock.position = CGPoint(x:(CGRectGetMinX(blockSize) + CGRectGetWidth(blockSize)/2), y: CGRectGetMinY(blockSize) + CGRectGetHeight(blockSize)/2)
+
+        return _androidBlock
+    }
+
+
+    enum AndroidMove {
+        case Left,Right
+    }
+
+    var nextMove = AndroidMove.Left
+    var lastAndroidsMovement  = 0 as CFTimeInterval
+    var stepDistance = 8 as CGFloat
+    func updateAndroidsPosition(currentTime: CFTimeInterval) {
+        if  ((currentTime - lastAndroidsMovement) < 0.30 as CFTimeInterval) {
+            return
+        }
+        let androids = self.childNodeWithName("Androids") as SKNode!
+        let androidsFrame = androids.calculateAccumulatedFrame()
+        if(nextMove == AndroidMove.Left) {
+            if((CGRectGetMinX(androidsFrame) - stepDistance) < CGRectGetMinX(self.frame)) {
+                androids.position = CGPoint(x: androids.position.x,y: (androids.position.y - stepDistance))
+                nextMove = AndroidMove.Right
+            } else {
+                androids.position = CGPoint(x: (androids.position.x - stepDistance), y:androids.position.y)
+            }
+        } else if(nextMove == AndroidMove.Right) {
+            if((CGRectGetMaxX(androidsFrame) + stepDistance) > CGRectGetMaxX(self.frame)) {
+                androids.position = CGPoint(x: androids.position.x,y: (androids.position.y - stepDistance))
+                nextMove = AndroidMove.Left
+            } else {
+                androids.position = CGPoint(x: (androids.position.x + stepDistance),y:androids.position.y)
+            }
+        }
+        self.lastAndroidsMovement = currentTime
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
@@ -48,5 +110,6 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        updateAndroidsPosition(currentTime)
     }
 }
